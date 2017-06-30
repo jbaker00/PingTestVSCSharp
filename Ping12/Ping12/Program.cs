f5c68dd39bfa171426449248360d1a677d9ee8c7
jbaker00 <jbaker00@gmail.com>
2017-06-30 10:36:36 -0700

    test

---
+++ Ping12/Ping12/Program.cs
@@ -0,0 +1,13 @@
+﻿//test
+using System;
+
+namespace Ping12
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
